eaa3a42e754a160609241c921ca203c3a178e3f5
Kévin <advestankv@gmail.com>
2024-11-20 11:37:51 +0100

    Checkpoint 3 Camera

--- Source/SmashUE/Private/Camera/CameraWorldSubsystem.cpp
+++ Source/SmashUE/Private/Camera/CameraWorldSubsystem.cpp
@@ -14,6 +14,12 @@ void UCameraWorldSubsystem::OnWorldBeginPlay(UWorld& InWorld)
 {
 	Super::OnWorldBeginPlay(InWorld);
 	CameraMain = FindCameraByTag(TEXT("CameraMain"));
+
+	AActor* CameraBoundsActor = FindCameraBoundsActor();
+	if(CameraBoundsActor != nullptr)
+	{
+		InitCameraBounds(CameraBoundsActor);
+	}
 }
 
 void UCameraWorldSubsystem::Tick(float DeltaTime)
@@ -32,10 +38,97 @@ void UCameraWorldSubsystem::RemoveFollowTarget(UObject* FollowTarget)
 	FollowTargets.Remove(FollowTarget);
 }
 
+AActor* UCameraWorldSubsystem::FindCameraBoundsActor()
+{
+	TArray<AActor*> CameraBounds;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), "CameraBounds", CameraBounds);
+	return CameraBounds.Num() > 0 ? CameraBounds[0] : nullptr;
+}
+
+void UCameraWorldSubsystem::InitCameraBounds(AActor* CameraBoundsActor)
+{
+	FVector BoundsCenter;
+	FVector BoundsExtents;
+	CameraBoundsActor->GetActorBounds(false, BoundsCenter, BoundsExtents);
+
+	CameraBoundsYProjectionCenter = BoundsCenter.Z;
+	CameraBoundsMin = FVector2D(BoundsCenter.X - BoundsExtents.X, BoundsCenter.Z - BoundsExtents.Z);
+	CameraBoundsMax = FVector2D(BoundsCenter.X + BoundsExtents.X, BoundsCenter.Z + BoundsExtents.Z);
+}
+
+void UCameraWorldSubsystem::ClampPositionIntoCameraBounds(FVector& Position)
+{
+	FVector2D ViewportBoundsMin,ViewportBoundsMax;
+	GetViewportBounds(ViewportBoundsMin, ViewportBoundsMax);
+	
+	FVector WorldBoundsMin = CalculateWorldPositionFromViewportPosition(ViewportBoundsMin);
+	FVector WorldBoundsMax = CalculateWorldPositionFromViewportPosition(ViewportBoundsMax);
+	
+	if(Position.X + WorldBoundsMin.X < CameraBoundsMin.X)
+	{
+		Position.X = CameraBoundsMin.X - WorldBoundsMin.X;
+	} else if(Position.X + WorldBoundsMax.X >= CameraBoundsMax.X)
+	{
+		Position.X = CameraBoundsMax.X - WorldBoundsMax.X;
+	}
+
+	if(Position.Z + WorldBoundsMax.Z < CameraBoundsMin.Y)
+	{
+		Position.Z = CameraBoundsMin.Y - WorldBoundsMax.Z;
+	} else if(Position.Z + WorldBoundsMin.Z >= CameraBoundsMax.Y)
+	{
+		Position.Z = CameraBoundsMax.Y - WorldBoundsMin.Z;
+	}
+}
+
+void UCameraWorldSubsystem::GetViewportBounds(FVector2D& OutViewportBoundsMin, FVector2D& OutViewportBoundsMax)
+{
+	UGameViewportClient* ViewportClient = GetWorld()->GetGameViewport();
+	if(ViewportClient == nullptr) return;
+
+	FViewport* Viewport = ViewportClient->Viewport;
+	if(Viewport == nullptr) return;
+
+	FIntRect ViewRect(
+		Viewport->GetInitialPositionXY(),
+		Viewport->GetInitialPositionXY() + Viewport->GetSizeXY()
+	);
+	FIntRect ViewportRect = Viewport->CalculateViewExtents(CameraMain->AspectRatio, ViewRect);
+
+	OutViewportBoundsMin.X = ViewportRect.Min.X;
+	OutViewportBoundsMin.Y = ViewportRect.Min.Y;
+
+	OutViewportBoundsMax.X = ViewportRect.Max.X;
+	OutViewportBoundsMax.Y = ViewportRect.Max.Y;
+}
+
+FVector UCameraWorldSubsystem::CalculateWorldPositionFromViewportPosition(const FVector2D& ViewportPosition)
+{
+	if(CameraMain == nullptr) return FVector::Zero();
+
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if(PlayerController == nullptr) return FVector::Zero();
+
+	float YDistanceToCenter = CameraMain->GetOwner()->GetActorLocation().Y - CameraBoundsYProjectionCenter;
+
+	FVector CameraWorldProjectDir;
+	FVector WorldPosition;
+	UGameplayStatics::DeprojectScreenToWorld(
+		PlayerController,
+		ViewportPosition,
+		WorldPosition,
+		CameraWorldProjectDir
+	);
+
+	WorldPosition += CameraWorldProjectDir * YDistanceToCenter;
+	return WorldPosition;
+}
+
 void UCameraWorldSubsystem::TickUpdateCameraPosition(float DeltaTime)
 {
 	if(CameraMain == nullptr) return;
 	FVector AveragePosition = CalculateAveragePositionBetweenTargets();
+	ClampPositionIntoCameraBounds(AveragePosition);
 	CameraMain->SetWorldLocation(AveragePosition);
 }
 
--- Source/SmashUE/Public/Camera/CameraWorldSubsystem.h
+++ Source/SmashUE/Public/Camera/CameraWorldSubsystem.h
@@ -15,22 +15,62 @@ class SMASHUE_API UCameraWorldSubsystem : public UTickableWorldSubsystem
 {
 	GENERATED_BODY()
 
+#pragma region Subsystem Override
+	
 public:
 	virtual void PostInitialize() override;
 	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
 	virtual void Tick(float DeltaTime) override;
 	virtual TStatId GetStatId() const override { return TStatId();}
+	
+#pragma endregion
+
+#pragma region Follow Targets
+	
+public:
 	void AddFollowTarget(UObject* FollowTarget);
 	void RemoveFollowTarget(UObject* FollowTarget);
+	
+#pragma endregion
+
+#pragma region Main Camera
 
 protected:
 	UPROPERTY()
 	TObjectPtr<UCameraComponent> CameraMain;
 
+#pragma endregion
+
+#pragma region Bounds
+
+protected:
+	UPROPERTY()
+	FVector2D CameraBoundsMin;
+
+	UPROPERTY()
+	FVector2D CameraBoundsMax;
+
+	UPROPERTY()
+	float CameraBoundsYProjectionCenter;
+
+	AActor* FindCameraBoundsActor();
+	void InitCameraBounds(AActor* CameraBoundsActor);
+	void ClampPositionIntoCameraBounds(FVector& Position);
+	void GetViewportBounds(FVector2D& OutViewportBoundsMin, FVector2D& OutViewportBoundsMax);
+	FVector CalculateWorldPositionFromViewportPosition(const FVector2D& ViewportPosition);
+	
+#pragma endregion 
+
+#pragma region Misc
+	
+protected:
 	UPROPERTY()
 	TArray<UObject*> FollowTargets;
 
 	void TickUpdateCameraPosition(float DeltaTime);
 	FVector CalculateAveragePositionBetweenTargets();
 	UCameraComponent* FindCameraByTag(const FName& Tag) const;
+
+#pragma endregion
+	
 };
